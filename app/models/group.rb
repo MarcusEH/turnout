45948981a0ab86d536904a63c8b99acd2459bb50
Marcus Hamill <marcusehamill@gmail.com>
2018-11-25 15:58:07 -0600

    Cleaned up group.rb methods for finding user openings and user interests for the group and removed user_groups_id column from users

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,21 +3,31 @@ class Group < ApplicationRecord
   has_many :users, through: :user_groups
   has_many :group_events
   has_many :user_interests, through: :users
+  has_many :user_calendar_openings, through: :users
 
   def find_opening
   #   time_range = 
     @begin_time = "1111-11-11T11:11:11"
     @end_time = "1111-11-11T11:11:11"
-    self.users.each do |user1|
-      self.users.each do |user2|
-        user1.user_calendar_openings.each do |opening_user1|
-          user2.user_calendar_openings.each do |opening_user2|
-            if (opening_user1.begin_time < opening_user2.end_time) && (opening_user2.begin_time < opening_user1.end_time)
-              @begin_time = opening_user2.begin_time
-              @end_time = opening_user2.end_time
-              p @end_time
-            end
-          end
+    # self.users.each do |user1|
+    #   self.users.each do |user2|
+    #     user1.user_calendar_openings.each do |opening_user1|
+    #       user2.user_calendar_openings.each do |opening_user2|
+    #         if (opening_user1.begin_time < opening_user2.end_time) && (opening_user2.begin_time < opening_user1.end_time)
+    #           @begin_time = opening_user2.begin_time
+    #           @end_time = opening_user2.end_time
+    #           p @end_time
+    #         end
+    #       end
+    #     end
+    #   end
+    # end
+    self.user_calendar_openings.each do |opening_user1|
+      self.user_calendar_openings.each do |opening_user2|
+        if (opening_user1.begin_time < opening_user2.end_time) && (opening_user2.begin_time < opening_user1.end_time)
+          @begin_time = opening_user2.begin_time
+          @end_time = opening_user2.end_time
+          p @end_time
         end
       end
     end
@@ -26,45 +36,30 @@ class Group < ApplicationRecord
   end
 
   def find_category
-    p "*" * 50
-    sum_sports = [0, "sports"]
-    sum_movies = [0, "movies"]
-    sum_music = [0, "music"]
-    sum_food = [0, "food"]
-    sum_special = [0, "special"]
-    sum_custom = [0, "custom"]
-    # sums = {"sports" => 0, "movies" => 0, "music" => 0, "food" => 0, "special" => 0, "custom" => 0}
+    sums = {"sports" => 0, "movies" => 0, "music" => 0, "food" => 0, "special" => 0, "custom" => 0}
     self.user_interests.each do |interest|
       if interest.category == "sports"
-        sum_sports[0] += interest.interest_level
+        sums["sports"] += interest.interest_level
       elsif interest.category == "movies"
-        sum_movies[0] += interest.interest_level
+        sums["movies"] += interest.interest_level
       elsif interest.category == "music"
-        sum_music[0] += interest.interest_level
+        sums["music"] += interest.interest_level
       elsif interest.category == "food"
-        sum_food[0] += interest.interest_level 
+        sums["food"] += interest.interest_level 
       elsif interest.category == "special"
-        sum_special[0] += interest.interest_level
+        sums["special"] += interest.interest_level
       elsif interest.category == "custom"
-        sum_custom[0] += interest.interest_level
+        sums["custom"] += interest.interest_level
       end
-      
     end
-    interests_array = [sum_sports, sum_movies, sum_music, sum_food, sum_special, sum_custom]
-    p interests_array
-    p "*" * 60
-    index = 0
     event_type = ""
     final_sum = 0
-    interests_array.each do |sum| 
-      
-      if final_sum < sum[0]
-        event_type = interests_array[index][1]
-        
+    sums.each do |key, value|
+      if final_sum < value
+        final_sum = value
+        event_type = key
       end
-      index += 1
     end
-    p event_type
     return event_type
   end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_235402) do
+ActiveRecord::Schema.define(version: 2018_11_25_215443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 2018_11_21_235402) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.integer "user_groups_id"
     t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
